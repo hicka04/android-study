d1c9ae9955293441a85a94a4ef7a9f929d3dc1f0
hicka04 <hicka04@gmail.com>
2021-02-27 17:36:31 +0900

    ViewBindingでAPIの結果を表示

--- app/src/main/java/com/example/androidstudy/scenes/searchresult/SearchResultFragment.kt
+++ app/src/main/java/com/example/androidstudy/scenes/searchresult/SearchResultFragment.kt
@@ -1,31 +1,24 @@
 package com.example.androidstudy.scenes.searchresult
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.androidstudy.R
+import com.example.androidstudy.databinding.SearchResultFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SearchResultFragment: Fragment() {
+class SearchResultFragment: Fragment(R.layout.search_result_fragment) {
     private val viewModel: SearchResultViewModel by viewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.search_result_fragment, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val binding = SearchResultFragmentBinding.bind(view)
+
         viewModel.articles.observe(viewLifecycleOwner) {
-            Log.d("SearchResultFragment", it.toString())
+            binding.text.text = it.toString()
         }
 
         viewModel.onViewCreated()
