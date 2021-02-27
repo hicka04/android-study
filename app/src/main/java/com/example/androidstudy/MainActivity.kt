d7bc8a28adcc152264e79d7a9862341211363946
hicka04 <hicka04@gmail.com>
2021-02-27 22:19:11 +0900

    不要な記述削除

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -1,17 +1,7 @@
 package com.example.androidstudy
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.androidstudy.repositories.qiita.QiitaRepository
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    @Inject lateinit var qiitaRepository: QiitaRepository
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
